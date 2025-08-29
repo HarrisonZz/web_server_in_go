56dc7a18fc334119b670dc84b04c549d98672fc7
Harrison <harrison_0418@outlook.com>
2025-08-30 00:29:36 +0800

    Add container build for armv7 platform image and binaries

--- go_build.sh
+++ go_build.sh
@@ -2,17 +2,26 @@
 
 IMAGE_NAME="go_build"
 CONTAINER_NAME="go_build_for_bbb"
+DOCKER_HUB="harrisonchen0418"
 
-if [ -z "$(docker images -q $IMAGE_NAME)" ]; then
-  echo "image '$IMAGE_NAME' not found, will build it first:" && \
-  docker build -t $IMAGE_NAME --target bbb-builder .
-fi
 
-docker run -v $PWD:/src --name $CONTAINER_NAME \
-    -w /src \
-    $IMAGE_NAME \
-    go build -buildvcs=false -o app-bbb.bin . && \
-docker logs $CONTAINER_NAME && \
-docker rm $CONTAINER_NAME
-
-mv app-bbb.bin ./out/
+case "$1" in
+  bin)
+    echo "[*] Building ARMv7 binary only..."
+    if [ -z "$(docker images -q $IMAGE_NAME)" ]; then
+      echo "image '$IMAGE_NAME' not found, will build it first:" && \
+      docker build -f Dockerfile.build -t $IMAGE_NAME --target bbb-builder .
+    fi
+    docker run --rm -v $PWD:/src --name $CONTAINER_NAME \
+    -w /src $IMAGE_NAME go build -buildvcs=false -o app-bbb.bin . && \
+    mv app-bbb.bin ./out/
+    ;;
+  image)
+    echo "Building full BBB runtime image..."
+    docker buildx build --platform linux/arm/v7 -f Dockerfile.runtime --push -t $DOCKER_HUB/$IMAGE_NAME:BBB .
+    ;;
+  *)
+    echo "Usage: $0 {bin|image}"
+    exit 1
+    ;;
+esac
