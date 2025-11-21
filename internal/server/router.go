cc1ae136c65d08ebcb1939826edb18f26cf9f65d
Harrison <harrison_0418@outlook.com>
2025-11-22 03:06:00 +0800

    add OTel span middleware

--- internal/server/router.go
+++ internal/server/router.go
@@ -3,13 +3,16 @@ package server
 import (
 	"github.com/HarrisonZz/web_server_in_go/internal/deps"
 	"github.com/HarrisonZz/web_server_in_go/internal/handler"
+	"github.com/HarrisonZz/web_server_in_go/internal/telemetry"
 	"github.com/gin-gonic/gin"
+
+	"go.opentelemetry.io/contrib/instrumentation/github.com/gin-gonic/gin/otelgin"
 )
 
 func NewRouter(d deps.Deps) *gin.Engine {
 	// 建 Router：預設含 Logger/Recovery 中介層
 	r := gin.New()
-	r.Use(gin.Logger(), gin.Recovery(), deps.InjectDeps(d))
+	r.Use(otelgin.Middleware("web-server-in-go"), telemetry.GinChildSpan(), gin.Logger(), gin.Recovery(), deps.InjectDeps(d))
 	r.NoRoute(handler.NoRoute)
 
 	routeMap := handler.GetRoutes()
